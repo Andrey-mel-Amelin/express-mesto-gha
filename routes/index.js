a1a5e0a6e6a2e9df8055465c914159d71cb19b5b
Andrey Amelin <more207@mail.ru>
2022-11-04 19:08:50 +0300

    feat: сделай логгер

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
-const { validAuthName } = require('../middlewares/joiValidation');
-const NotFoundError = require('../errors/NotFoundError');
 
+const NotFoundError = require('../errors/NotFoundError');
 const { login, createUser } = require('../controllers/users');
+const { validAuthName } = require('../middlewares/joiValidation');
 const auth = require('../middlewares/auth');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+
+router.use(requestLogger);
 
 router.post('/signin', validAuthName, login);
 router.post('/signup', validAuthName, createUser);
 router.use(auth);
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
+
+router.use(errorLogger);
 
 router.use((req, res, next) => {
   next(new NotFoundError('Маршрут не найден.'));
