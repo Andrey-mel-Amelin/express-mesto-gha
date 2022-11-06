48ba9de10738574cdb27a076ffcf7395b0bc1fda
Andrey Amelin <more207@mail.ru>
2022-11-07 00:18:33 +0300

    fix: исправь CORS v6

--- constants/index.js
+++ constants/index.js
@@ -5,9 +5,7 @@ const REGEX_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{
 const VALIDATION_ERROR = 'Validation failed';
 const allowedCors = [
   'https://amelin.mesto.nomoredomains.icu,',
-  'https://amelin.mesto.backend.nomoredomains.icu',
   'http://amelin.mesto.nomoredomains.icu,',
-  'http://amelin.mesto.backend.nomoredomains.icu',
   'https://localhost:3000',
   'http://localhost:3000',
   'https://127.0.0.1:3000',
