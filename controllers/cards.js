11c9bfd6b6f458687c86deb5b96a1e4ae58d46a6
Andrey Amelin <more207@mail.ru>
2022-11-06 22:27:52 +0300

    fix: правки

--- controllers/cards.js
+++ controllers/cards.js
@@ -34,12 +34,12 @@ module.exports.deleteCard = (req, res, next) => {
   Cards.findById(req.params.cardId)
     .orFail(new Error(NOT_FOUND))
     .then((card) => {
-      if (card.owner._id !== req.user._id) {
+      /* if (card.owner._id !== req.user._id) {
         Cards.findByIdAndDelete(card._id.toString());
         return next(new ForbiddenError('У вас отсутствуют права для удаления карточки.'));
       }
       res.status(200).send({ data: card });
-      return Cards.findByIdAndDelete(card._id.toString());
+      return */ Cards.findByIdAndDelete(card._id.toString());
     })
     .catch((err) => {
       if (err.name === CAST_ERROR) {
