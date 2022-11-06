9b210e5b463af0dd13572a51ee502f4fb1113ac4
Andrey Amelin <more207@mail.ru>
2022-11-07 00:48:12 +0300

    fix: исправь CORS v9

--- middlewares/allowedCors.js
+++ middlewares/allowedCors.js
@@ -1,24 +1,18 @@
 const allowedCors = [
-  'https://amelin.mesto.nomoredomains.icu,',
-  'http://amelin.mesto.nomoredomains.icu,',
-  'https://localhost:3000',
-  'http://localhost:3000',
-  'https://127.0.0.1:3000',
-  'http://127.0.0.1:3000',
+  'https://amelin.mesto.nomoredomains.icu',
+  'http://amelin.mesto.nomoredomains.icu',
+  'localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  console.log(origin)
   const requestHeaders = req.headers['access-control-request-headers'];
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
-    console.log(1)
+    console.log(origin);
     res.header('Access-Control-Allow-Origin', origin);
-  } else {
-    console.log(2)
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
