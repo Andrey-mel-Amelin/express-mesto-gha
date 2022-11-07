d8ec824a5a31523654b51253b5d191f80173fb53
Andrey Amelin <more207@mail.ru>
2022-11-07 22:27:10 +0300

    fix: исправить после ревью

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,4 @@
-const NOT_FOUND = 'NotFound';
 const CAST_ERROR = 'CastError';
-const ERROR_EMAIL_OR_PASSWORD = 'Неправильные почта или пароль';
 const REGEX_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
 const VALIDATION_ERROR = 'Validation failed';
 const allowedCors = [
@@ -10,9 +8,7 @@ const allowedCors = [
 ];
 
 module.exports = {
-  NOT_FOUND,
   CAST_ERROR,
-  ERROR_EMAIL_OR_PASSWORD,
   REGEX_URL,
   VALIDATION_ERROR,
   allowedCors,
